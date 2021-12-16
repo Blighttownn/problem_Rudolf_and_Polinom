2bd6b0a469f75d033024923609e834ca7a78e179
Blighttown <kartoshkalex2004@gmail.com>
2021-12-17 01:02:29 +0300

    add pre-final

--- main.py
+++ main.py
@@ -1,55 +1,93 @@
-maximum_number_degree = int(input())
-coefficients_of_numbers = [int(input()) for i in range(maximum_number_degree + 1)]
-polinom = ""
-count_of_degree = 0
-for number in range(len(coefficients_of_numbers)):
-    if coefficients_of_numbers[number] == 0:
+import time
+
+
+def main():
+    maximum_number_degree = int(input())
+    coefficients_of_numbers = [int(input()) for i in range(maximum_number_degree + 1)]
+    start_time = time.time()
+    polinom = ""
+    count_of_degree = 0
+    for i in range(len(coefficients_of_numbers)):
+        polinom += build(coefficients_of_numbers[i], maximum_number_degree - count_of_degree,
+                         count_of_degree)
         count_of_degree += 1
-        continue
-    if maximum_number_degree - count_of_degree > 1 and maximum_number_degree - count_of_degree != maximum_number_degree:
-        if coefficients_of_numbers[number] != 1 and coefficients_of_numbers[number] != -1:
-            if coefficients_of_numbers[number] > 0:
-                polinom += " " + "+" + " " + str(coefficients_of_numbers[number]) + " " + "*" + " " + "x" + "^" + str(
-                    maximum_number_degree - count_of_degree)
-            else:
-                polinom += " " + "-" + " " + str(
-                    abs(coefficients_of_numbers[number])) + " " + "*" + " " + "x" + "^" + str(
-                    maximum_number_degree - count_of_degree)
-        if coefficients_of_numbers[number] == 1:
-            polinom += " " "+" + " " + "x" + "^" + str(maximum_number_degree - count_of_degree)
-        if coefficients_of_numbers[number] == -1:
-            polinom += " " + "-x" + "^" + str(maximum_number_degree - count_of_degree)
-    #
-    if maximum_number_degree - count_of_degree == maximum_number_degree:
-        if coefficients_of_numbers[number] != 1 and coefficients_of_numbers[number] != -1:
-            polinom += str(coefficients_of_numbers[number]) + " " + "*" + " " + "x" + "^" + str(
-                maximum_number_degree - count_of_degree)
-        if coefficients_of_numbers[number] == 1:
-            polinom += "x" + "^" + str(maximum_number_degree - count_of_degree)
-        if coefficients_of_numbers[number] == -1:
-            polinom += "-" + "x" + "^" + str(maximum_number_degree - count_of_degree)
-    #
-    if maximum_number_degree - count_of_degree == 1:
-        if coefficients_of_numbers[number] != 1 and coefficients_of_numbers[number] != -1:
-            if coefficients_of_numbers[number] > 0:
-                polinom += " " + "+" + " " + str(coefficients_of_numbers[number]) + " " + "*" + " " + "x"
-            else:
-                polinom += " " + "-" + " " + str(abs(coefficients_of_numbers[number])) + " " + "*" + " " + "x"
-        else:
-            if coefficients_of_numbers[number] > 0:
-                polinom += "+" + "x"
-            else:
-                polinom += "x"
-        if coefficients_of_numbers[number] == -1:
-            polinom += " " + "-" + " " + "x"
+    print("время выполнения: ")
+    print(time.time() - start_time)
+    if polinom == "":
+        print(0)
+    else:
+        print(polinom)
 
-    if maximum_number_degree - count_of_degree == 0:
-        if coefficients_of_numbers[number] > 0:
-            polinom += " " + "+" + " " + str(coefficients_of_numbers[number])
-        else:
-            polinom += " " + "-" + " " + str(abs(coefficients_of_numbers[number]))
 
-    count_of_degree += 1
-if polinom == "":
-    print(0)
-print(polinom)
+def zero_coeffic():
+    return ""
+
+
+def first_monom(coeffic, max_deg):
+    result = ""
+    if coeffic < 0:
+        result += "-"
+    if coeffic != 1:
+        result += str(coeffic)
+    if max_deg != 0:
+        if result != "":
+            result += " * "
+        result += f'x{degrees(max_deg, coeffic)}'
+
+    return result
+
+
+def degrees(degree, coeffic):
+    if degree > 1:
+        return "^" + str(degree)
+    if degree == 1:
+        return ""
+    if degree == 0:
+        return degree_zero(coeffic)
+
+
+def degree_zero(coeffic):
+    return " " + plus_or_minus(coeffic) + " " + str(abs(int(coeffic)))
+
+
+def plus_or_minus(coeffic):
+    if coeffic > 0:
+        return "+"
+    if coeffic < 0:
+        return "-"
+    else:
+        zero_coeffic()
+
+
+def one_coeffic(coeffic, degree):
+    if degree != 0:
+        return " " + plus_or_minus(coeffic) + " " + "x" + degrees(degree, coeffic)
+    else:
+        return degree_zero(coeffic)
+
+
+def big_coeffic(coeffic, degree):
+    if degree != 0:
+        return " " + plus_or_minus(coeffic) + " " + str(abs(coeffic)) + " " + "*" + " " + "x" + degrees(degree,
+                                                                                                        coeffic)
+    else:
+        return degree_zero(coeffic)
+
+
+def build(coeffic, degree, cnt_deg):
+    if cnt_deg == 0:
+        return first_monom(coeffic, degree)
+    elif coeffic == 0:
+        return zero_coeffic()
+    elif coeffic == 1:
+        return one_coeffic(coeffic, degree)
+    elif coeffic > 1:
+        return big_coeffic(coeffic, degree)
+    elif coeffic < -1:
+        return big_coeffic(coeffic, degree)
+    elif coeffic == -1:
+        return one_coeffic(coeffic, degree)
+
+
+if __name__ == "__main__":
+    main()
